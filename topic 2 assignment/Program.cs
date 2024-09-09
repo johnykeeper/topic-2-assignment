7a8fe00d65e8b1e6adcbb74e12e172765cc6b5c2
Jonathan Gaboury <362568651@GoTVDSB.ca>
2024-09-09 09:11:59 -0400

    Add project files.

---
+++ topic 2 assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace topic_2_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // jonathan
+            Console.WriteLine("2009");
+            Console.WriteLine();
+            Console.WriteLine((14.0 + 15.0 + 16.0) / 3);
+            Console.WriteLine();
+            Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0) / 3);
+            Console.WriteLine();
+            Console.WriteLine(3.14*(3 * 3));
+            Console.WriteLine();
+            Console.WriteLine("if both the numbers are outside the quotations it will add them instead of putting them next to eachother like this "+1234);
+            Console.WriteLine();
+        }
+    }
+}
